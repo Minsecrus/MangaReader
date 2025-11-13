bdccab8f1347828b15adf6a4ea8480089d08343b
Malloy <li3210361050@gmail.com>
2025-11-13 17:33:16 +0800

    feat: 添加了CopyButton的样式 使得复制操作变得可见

--- app/components/CopyButton.vue
+++ app/components/CopyButton.vue
@@ -1,5 +1,6 @@
 <!-- app/components/CopyButton.vue -->
 <script setup lang="ts">
+const buttonText = ref('复制')
 // 接收一个父级传递过来的text来实现点击按钮复制
 interface Prop {
     textToCopy: string
@@ -9,14 +10,21 @@ const { textToCopy } = defineProps<Prop>()
 const copyText = async () => {
     try {
         await window.navigator.clipboard.writeText(textToCopy)
+        buttonText.value = '已复制'
     } catch (err) {
+        buttonText.value = '复制失败'
         console.error('复制失败:', err)
+    } finally {
+        setTimeout(() => {
+            buttonText.value = '复制'
+        }, 1500)
     }
 }
 </script>
 
 <template>
-    <button class="text-xs px-2 py-1 rounded cursor-pointer transition-all hover:opacity-80 bg-manga-400 text-white" @click="copyText">
-        复制
+    <button class="text-xs px-2 py-1 rounded cursor-pointer transition-all hover:opacity-80 bg-manga-400 text-white"
+        @click="copyText">
+        {{ buttonText }}
     </button>
 </template>
